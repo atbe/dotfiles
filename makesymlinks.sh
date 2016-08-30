8fe989f8771cef88810c5b5ab5b65a9dfe7191b7
@be <abeahmed2@gmail.com>
2016-08-29 22:06:42 -0400

    Allow shared dofiles such as gitconfig.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -22,7 +22,8 @@ echo "You are running $OSTYPE"
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc tmux.conf bashrc config.fish"    # list of files/folders to symlink in homedir
+os_specific_files="vimrc tmux.conf bashrc config.fish"    # list of files/folders to symlink in homedir
+global_files="gitconfig"      # Any configs shared by mac and linux
 
 ##########
 
@@ -37,10 +38,17 @@ cd $dir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
+for file in $os_specific_files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
 	mv ~/.$file ~/dotfiles_old/
 	echo "Creating symlink to $file in home directory."
 	if_os linux && ln -s $dir/linux/$file ~/.$file && echo "LINUX"
 	if_os darwin && ln -s $dir/mac/$file ~/.$file && echo "MAC"
+done
+
+for file in $global_files; do
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old/
+	echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file
 done
