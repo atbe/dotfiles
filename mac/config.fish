647b5cf654bac5b67948576d186ee2e04f093b69
Ibrahim Ahmed <abeahmed2@gmail.com>
2026-08-02 11:28:33 -0700

    Make dotfiles home-directory portable
    Replace machine-specific home paths and non-expanding quoted tildes with HOME-based paths across the macOS and Linux shell configurations, Fish setup, and symlink installer. This keeps SDKs, package managers, completions, and local CLI paths valid for the active user on any machine.\n\nAlso retain the existing nvm default-version selection and pnpm PATH initialization that were present in the original unstaged zsh configuration.

--- mac/config.fish
+++ mac/config.fish
@@ -20,10 +20,10 @@
 # Fish
 #
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/abe/.local/share/omf"
+set -gx OMF_PATH "$HOME/.local/share/omf"
 #`
 # Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG "/Users/abe/.config/omf"
+set -gx OMF_CONFIG "$HOME/.config/omf"
 #
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -20,8 +20,8 @@ echo "You are running $OSTYPE"
 
 ########## Variables
 
-dir=~/dotfiles                                         # dotfiles directory
-olddir=~/dotfiles_old                                  # old dotfiles backup directory
+dir="$HOME/dotfiles"                                  # dotfiles directory
+olddir="$HOME/dotfiles_old"                           # old dotfiles backup directory
 os_specific_files="tmux.conf bashrc config.fish zshrc" # list of files/folders to symlink in homedir
 global_files="vimrc gitconfig zsh_plugins"                         # Any configs shared by mac and linux
 
@@ -29,24 +29,24 @@ global_files="vimrc gitconfig zsh_plugins"                         # Any configs
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+mkdir -p "$olddir"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
-cd $dir
+cd "$dir"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $os_specific_files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv -f ~/.$file ~/dotfiles_old/
+	mv -f "$HOME/.$file" "$olddir/"
 	echo "Creating symlink to $file in home directory."
-	if_os linux && ln -s $dir/linux/$file ~/.$file && echo "LINUX"
-	if_os darwin && ln -s $dir/mac/$file ~/.$file && echo "MAC"
+	if_os linux && ln -s "$dir/linux/$file" "$HOME/.$file" && echo "LINUX"
+	if_os darwin && ln -s "$dir/mac/$file" "$HOME/.$file" && echo "MAC"
 done
 
 for file in $global_files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv -f ~/.$file ~/dotfiles_old/ 
+	mv -f "$HOME/.$file" "$olddir/"
 	echo "Creating symlink to $file in home directory."
-	ln -s $dir/global/$file ~/.$file
+	ln -s "$dir/global/$file" "$HOME/.$file"
 done
